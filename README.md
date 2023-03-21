f44a4b1d440a415e1639a38e9496c0831d437f08
Luís Otávio Amorim <luisotaviolamorim@gmail.com>
2023-03-20 22:29:45 -0300

    Added data fetch function
    Function that receives the API result and return character data created.
    This function will be used to iterate threw the API results and create
    all the desired dataframe rows.

--- README.md
+++ README.md
@@ -4,9 +4,31 @@
 - Generate API keys on [marvel's website](https://developer.marvel.com/)
 - Put the keys on the correct variables on `keys.py`
 - Run `main.py` to get a sample of the data
+- The file `fetched_data.csv` will contain the desired data from the first 100 characters in alphabetical order
 
 ## Development strategy
-- Make a single API call
-- Understand the data format by fetching only one row
-- Write a script that get the needed info from the first row
+- Make a single API call ✓
+- Understand the data format by fetching only one row ✓
+- Write a script that get the needed info from the first row ✓
 - Write a script that requests all the dataset
+
+## API result understanding
+The return of the request is a JSON file, the desired data is inside the "results" field. The value of this field is a
+list of other key-value mapped objects.
+
+Each of those key-value objects represent a different character with all the requested fields. The `id`, `name`,
+`description` data are contained directly as values mapped but keys with the same names. But the `comics`, `series`,
+`stories` and `events` values are contained inside another object as the available key. 
+
+So, using the python dictionary syntax to get the data from a json file, and considering the hero variable the object of
+a random hero, we can access all the needed properties as shown below.
+
+```python
+hero_id = hero['id']
+hero_name = hero['name']
+hero_description = hero['description']
+numer_of_comics = hero['comics']['available']
+numer_of_series = hero['series']['available']
+numer_of_stories = hero['stories']['available']
+numer_of_events = hero['events']['available']
+```
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import json
 import random
 import string
 from hashlib import md5
-import json
 
+import pandas as pd
 import requests
 
 from keys import Keys
@@ -18,22 +19,47 @@ def md5_checksum(pub_key, priv_key, ts):
     return md5(codificada).hexdigest()
 
 
-# API url
-base_url = 'https://gateway.marvel.com:443/'
-endpoint = '/v1/public/characters?'
-url = base_url + endpoint
-
-# Auth
-ts = generate_ts()
-auth_hash = md5_checksum(Keys.pub_key, Keys.priv_key, ts)
-
-# Request
-param = {'ts': ts, 'apikey': Keys.pub_key, 'hash': auth_hash, 'limit': 1}
-r = requests.get(url, params=param)
-
-dados = r.json().get('data').get('results')
-
-with open("sample_data.json", mode='w') as outfile:
-    json.dump(dados[0], outfile)
+def get_character_data(character_json):
+    metadata = 'id', 'name', 'description'
+    data = 'comics', 'series', 'stories', 'events'
+    character_data = [character_json.get(value) for value in metadata]
+    for value in data:
+        character_data.append(character_json.get(value).get('available'))
+    return character_data
 
 
+def api_call(offset=0, pub_key=Keys.pub_key, priv_key=Keys.priv_key):
+    # API url
+    base_url = 'https://gateway.marvel.com:443/'
+    endpoint = '/v1/public/characters?'
+    url = base_url + endpoint
+
+    # Auth
+    ts = generate_ts()
+    auth_hash = md5_checksum(pub_key, priv_key, ts)
+
+    # Request
+    param = {
+        'ts': ts,
+        'apikey': pub_key,
+        'hash': auth_hash,
+        'limit': 100,
+        'offset': offset,
+    }
+    r = requests.get(url, params=param)
+
+    return r.json().get('data').get('results')
+
+
+def main():
+    response = api_call()
+    result = []
+    for character in response:
+        result.append(get_character_data(character))
+
+    df = pd.DataFrame(result)
+    df.to_csv('fetched_data.csv')
+
+
+if __name__ == '__main__':
+    main()
